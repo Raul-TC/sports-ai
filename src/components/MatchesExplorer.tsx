cf32404e68f1744f63b91efff536a32c7f44ed25
Raul-TC <raultc.9819@gmail.com>
2026-08-01 07:09:57 -0600

    rootMargin:900px

--- src/components/MatchesExplorer.tsx
+++ src/components/MatchesExplorer.tsx
@@ -154,7 +154,7 @@ export default function MatchesExplorer({ predictions, results }: MatchesExplore
                     loadMore();
                 }
             },
-            { root: null, rootMargin: "500px", threshold: 0 }
+            { root: null, rootMargin: "900px", threshold: 0 }
         );
 
         observer.observe(loader);
